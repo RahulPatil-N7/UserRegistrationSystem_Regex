1ae37299caa4968583b2b2f1e50742dbb67f4494
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-11 01:43:53 +0530

    Added : User Registration System using regex.

---
+++ UserRegistrationSystem1/src/main/java/com/bridgelabz/UserEntry/UserRegistrationMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.UserEntry;
+
+public class UserRegistrationMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registration System.");
+
+	}
+
+}
