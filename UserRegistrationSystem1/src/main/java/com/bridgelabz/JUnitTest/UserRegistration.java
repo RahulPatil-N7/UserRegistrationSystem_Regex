9d10a50734c3d0fe1d9b9ac9525c7bcecb982dd5
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-16 01:24:29 +0530

    Added : JUnit test cases for user registration entry data.

--- UserRegistrationSystem1/src/main/java/com/bridgelabz/JUnitTest/UserRegistration.java
+++ UserRegistrationSystem1/src/main/java/com/bridgelabz/JUnitTest/UserRegistration.java
@@ -5,74 +5,76 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	//Uc1 - Validate first name using regex
-	public static String validFirstName(String firstName) {
+	//Uc1 - validate first name using regex
+	public static String HappyFirstName(String firstName) {
 		String regex = ("[A-Z]{1}[a-zA-Z]{2,}$");
 		
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
 		if (matcher.matches()) {
-			return "Valid";
+			return "Happy";
 		}
 		else {
-			return "InValid";
+			return "Sad";
 		}
 	}
 	
-	//Uc2 - Validate last name using regex
-	public static String validLastName(String lastName) {
+	//Uc2 - validate last name using regex
+	public static String HappyLastName(String lastName) {
 		String regex = ("[A-Z]{1}[a-zA-Z]{2,}$");
 			
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastName);
 		if (matcher.matches()) {
-			return "Valid";
+			return "Happy";
 		}
 		else {
-			return "InValid";
+			return "Sad";
 		}
 	}
 
-	//Uc3 - Validate email using regex
-	public static String validEMail(String eMail) {
+	//Uc3 - validate email using regex
+	public static String HappyEMail(String eMail) {
 		String regex = ("^[a-zA-Z0-9]{2,20}([.][a-z0-9]+)?@[a-z]+.[a-z]{2,3}(.[a-z]{2})?$");		
 		
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(eMail);
 		if (matcher.matches()) {
-			return "Valid";
+			return "Happy";
 		}
 		else {
-			return "InValid";
+			return "Sad";
 		}
 	}
 	
-	//Uc4,5,6,7- Validate password using regex - minimum 8 characters,at least one capital letter,number,special character
-	public static String validPassWord(String passWord) {
+	//Uc4,5,6,7- validate password using regex - minimum 8 characters,at least one capital letter,number,special character
+	public static String HappyPassWord(String passWord) {
 		String regex = ("(?=.*[A-Z]){1,}(?=.*[a-z]){1,}(?=.*[0-9]){1,}(?=.*[@#$%^&-+=()]){1}[a-zA-Z0-9@#$%^&-+=()]{8,}");
 					
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(passWord);
 		if (matcher.matches()) {
-			return "Valid";
+			return "Happy";
 		}
 		else {
-			return "InValid";
+			return "Sad";
 		}
 	}
 	
-	//Uc8 - Validate mobile number using regex
-	public static String validMobileNumber(String mobileNumber) {
+	//Uc8 - validate mobile number using regex
+	public static String HappyMobileNumber(String mobileNumber) {
 		String regex = ("[0-9]{2}[\\s][6789]{1}[0-9]{9}");
 					
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(mobileNumber);
 		if (matcher.matches()) {
-			return "Valid";
+			return "Happy";
 		}
 		else {
-			return "InValid";
+			return "Sad";
 		}
 	}
+	
+	
 
 }
--- UserRegistrationSystem1/src/main/java/com/bridgelabz/JUnitTest/UserRegistrationTest.java
+++ UserRegistrationSystem1/src/main/java/com/bridgelabz/JUnitTest/UserRegistrationTest.java
@@ -9,59 +9,59 @@ public class UserRegistrationTest {
 	 * First name should have first letter capital and
 	 * minimum characters */
 	@Test
-	public void checkIf_ReturnsTrue_IfFirstCharatcterIsCaptitalAndHasMinimumThreeCharactersInFirstName() {
+	public void givenString_ShoudReturnTrue_IfFirstCharatcterIsCaptitalAndHasMinimumThreeCharactersInFirstName() {
 		UserRegistration user = new UserRegistration();
 		
-		String actual = user.validFirstName("Rahul");
-		String expected = "Valid";
+		String actual = user.HappyFirstName("Rahul");
+		String expected = "Happy";
 		Assert.assertEquals(actual, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfFirstCharatcterIsNotCapitalInFirstName() {
+	public void givenString_ShouldReturnFalse_IfFirstCharatcterIsNotCapitalInFirstName() {
 		UserRegistration user = new UserRegistration();
 		
-		String actual = user.validFirstName("rahul");
-		String expected = "InValid";
+		String actual = user.HappyFirstName("rahul");
+		String expected = "Sad";
 		Assert.assertEquals(actual, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfThereAreLessThamThreeCharactersInFirstName() {
+	public void givenString_ShouldReturnFalse_IfThereAreLessThamThreeCharactersInFirstName() {
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validFirstName("Rh");
-		String expectedult = "InValid";
-		Assert.assertEquals(result, expectedult);
+		String result = user.HappyFirstName("Rh");
+		String expected = "Sad";
+		Assert.assertEquals(result, expected);
 	}
 	
 	/* Tests whether last name has proper format or not.
 	 * Last name should have first letter capital and
 	 * minimum characters */
 	@Test
-	public void checkIf_ReturnsTrue_IfFirstCharatcterIsCapitalHasMinimumThreeCharactersInLastName() {
+	public void givenString_ShouldReturnTrue_IfFirstCharatcterIsCapitalHasMinimumThreeCharactersInLastName() {
 		UserRegistration user = new UserRegistration();
 		
-		String actual = user.validFirstName("Patil");
-		String expected = "Valid";
+		String actual = user.HappyFirstName("Patil");
+		String expected = "Happy";
 		Assert.assertEquals(actual, expected);
 	}
 	
 	@Test
-	public void checkIf_ReturnsFalse_IfFirstCharatcterIsNotCapitalInLastName() {
+	public void givenString_ShouldReturnFalse_IfFirstCharatcterIsNotCapitalInLastName() {
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validLastName("patil");
-		String expected = "InValid";
+		String result = user.HappyLastName("patil");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfFirstCharatcterIsCapitalButHasLessThanThreeCharactersInLastName() {
+	public void givenString_ShouldReturnFalse_IfFirstCharatcterIsCapitalButHasLessThanThreeCharactersInLastName() {
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validLastName("Pa");
-		String expected = "InValid";
+		String result = user.HappyLastName("Pa");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
@@ -69,72 +69,72 @@ public class UserRegistrationTest {
 	 * EMail format-abc.xyz@bl.co.in where 
 	 * abc@bl.co is mandatory part and .xyz, .in are optional */
 	@Test
-	public void checkIf_ReturnsFalse_IfEmailDoesNotHaveAtSymbol() {
+	public void givenString_ShouldReturnFalse_IfEmailDoesNotHaveAtSymbol() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("abc.xyz.co.in");
-		String expected = "InValid";
+		String result = user.HappyEMail("abc.xyz.co.in");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfEmailHasMoreThanThreeTLD() {
+	public void givenString_ShouldReturnFalse_IfEmailHasMoreThanThreeTLD() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("abc.xyz@bl.co.in.co");
-		String expected = "InValid";
+		String result = user.HappyEMail("abc.xyz@bl.co.in.co");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfEmailHasOneCharacterTLD() {
+	public void givenString_ShouldReturnFalse_IfEmailHasOneCharacterTLD() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("abc.xyz@.co.i");
-		String expected = "InValid";
+		String result = user.HappyEMail("abc.xyz@.co.i");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfEmailHasTwoContinuousDots() {
+	public void givenString_ShouldReturnFalse_IfEmailHasTwoContinuousDots() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("abc..xyz@co.in");
-		String expected = "InValid";
+		String result = user.HappyEMail("abc..xyz@co.in");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfEmailHasThreeDotsBeforeAtSymbol() {
+	public void givenString_ShouldReturnFalse_IfEmailHasThreeDotsBeforeAtSymbol() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("alb.xyz.co@.in");
-		String expected = "InValid";
+		String result = user.HappyEMail("alb.xyz.co@.in");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfEmailHasDigitsInTLD() {
+	public void givenString_ShouldReturnFalse_IfEmailHasDigitsInTLD() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("abc.xyz@c1.in");
-		String expected = "InValid";
+		String result = user.HappyEMail("abc.xyz@c1.in");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsTrue_IfEmailHasProperFormatAsRequired() {
+	public void givenString_ShouldReturnTrue_IfEmailHasProperFormatAsRequired() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validEMail("abc.xyz@bl.co.in");
-		String expected = "Valid";
+		String result = user.HappyEMail("abc.xyz@bl.co.in");
+		String expected = "Happy";
 		Assert.assertEquals(result, expected);
 	}
 
@@ -142,42 +142,42 @@ public class UserRegistrationTest {
 	 * Mobile number should have space after country code
 	 * while country code is also mandatory */
 	@Test
-	public void checkIf_ReturnsTrue_IfMobileNumberHasSpaceAfterCountryCodeAndTenDigitsNumber() {
+	public void givenString_ShouldReturnTrue_IfMobileNumberHasSpaceAfterCountryCodeAndTenDigitsNumber() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validMobileNumber("91 9967116186");
-		String expected = "Valid";
+		String result = user.HappyMobileNumber("91 9967116186");
+		String expected = "Happy";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfMobileNumberDoesNotHaveSpaceAfterCountryCode() {
+	public void givenString_ShouldReturnFalse_IfMobileNumberDoesNotHaveSpaceAfterCountryCode() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validMobileNumber("919967116186");
-		String expected = "InValid";
-		Assert.assertEquals(result, expected);
-	}
-	
-	@Test
-	public void checkIf_ReturnsFalse_IfMobileNumberHasMoreThanTenDigits() {
-
-		UserRegistration user = new UserRegistration();
-		
-		String result = user.validMobileNumber("9199671161861");
-		String expected = "InValid";
+		String result = user.HappyMobileNumber("919967116186");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 	
 	@Test
-	public void checkIf_ReturnsFalse_IfMobileNumberHasLessThanTenDigits() {
+	public void givenString_ShouldReturnFalse_IfMobileNumberHasMoreThanTenDigits() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validMobileNumber("91996711618");
-		String expected = "InValid";
+		String result = user.HappyMobileNumber("9199671161861");
+		String expected = "Sad";
+		Assert.assertEquals(result, expected);
+	}
+	
+	@Test
+	public void givenString_ShouldReturnFalse_IfMobileNumberHasLessThanTenDigits() {
+
+		UserRegistration user = new UserRegistration();
+		
+		String result = user.HappyMobileNumber("91996711618");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
@@ -186,52 +186,53 @@ public class UserRegistrationTest {
 	 * lowercase & one speacial character. Minimum character
 	 * should be eight */
 	@Test
-	public void checkIf_ReturnsFalse_IfPassWordDoesNotHaveAtLeastOneUpperCaseLetter() {
+	public void givenString_ShouldReturnFalse_IfPassWordDoesNotHaveAtLeastOneUpperCaseLetter() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validMobileNumber("rahul@123");
-		String expected = "InValid";
+		String result = user.HappyMobileNumber("rahul@123");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfPassWordDoesNotHaveAtLeastOneSpecialCharacter() {
+	public void givenString_ShouldReturnFalse_IfPassWordDoesNotHaveAtLeastOneSpecialCharacter() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validPassWord("rahul123");
-		String expected = "InValid";
+		String result = user.HappyPassWord("rahul123");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfPassWordDoesNotHaveAtLeastOneLowerCaseLetter() {
+	public void givenString_ShouldReturnFalse_IfPassWordDoesNotHaveAtLeastOneLowerCaseLetter() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validPassWord("RAHUL@123");
-		String expected = "InValid";
+		String result = user.HappyPassWord("RAHUL@123");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsFalse_IfPassWordDoesNotHaveMinimumEightCharacters() {
+	public void givenString_ShouldReturnFalse_IfPassWordDoesNotHaveMinimumEightCharacters() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validPassWord("R@123");
-		String expected = "InValid";
+		String result = user.HappyPassWord("R@123");
+		String expected = "Sad";
 		Assert.assertEquals(result, expected);
 	}
 
 	@Test
-	public void checkIf_ReturnsTrue_IfPassWordIsAcceptedAsPerRequiredFormat() {
+	public void givenString_ShouldReturnTrue_IfPassWordIsAcceptedAsPerRequiredFormat() {
 
 		UserRegistration user = new UserRegistration();
 		
-		String result = user.validPassWord("Abccc@123");
-		String expected = "Valid";
+		String result = user.HappyPassWord("Abccc@123");
+		String expected = "Happy";
 		Assert.assertEquals(result, expected);
 	}
+	
 }
