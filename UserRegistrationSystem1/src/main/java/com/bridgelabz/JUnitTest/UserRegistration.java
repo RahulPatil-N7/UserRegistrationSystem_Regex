a1ae98a655fab6f141eb3deb97f10bd9c8b9bc73
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-27 00:21:01 +0530

    Refactored : Throwing custom exceptions for invalid user entry.

--- UserRegistrationSystem1/src/main/java/com/bridgelabz/JUnitTest/UserRegistration.java
+++ UserRegistrationSystem1/src/main/java/com/bridgelabz/JUnitTest/UserRegistration.java
@@ -7,97 +7,97 @@ import com.bridgelabz.UserEntry.UserRegistrationExceptions;
 public class UserRegistration {
 	
 	//Uc1 - validate first name using regex
-	public String validFirstName(String firstName) throws UserRegistrationExceptions {
+	public void validFirstName(String firstName) throws UserRegistrationExceptions {
 		String regex = ("[A-Z]{1}[a-zA-Z]{2,}$");
 		
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
 		try {
 			if (matcher.matches()) {
-				return "Name is valid";
+				System.out.println("Name is valid");
 			}
 			else {
 				throw new UserRegistrationExceptions("Name is invalid");
 			}
 		}
 		catch (UserRegistrationExceptions e) {
-			throw new UserRegistrationExceptions("Name is invalid");
+			System.out.println(e.toString());
 		}
 	}
 	
 	//Uc2 - validate last name using regex
-	public String validLastName(String lastName) throws UserRegistrationExceptions {
+	public void validLastName(String lastName) throws UserRegistrationExceptions {
 		String regex = ("[A-Z]{1}[a-zA-Z]{2,}$");
 			
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastName);
 		try {
 			if (matcher.matches()) {
-				return "Name is valid";
+				System.out.println("Name is valid");
 			}
 			else {
 				throw new UserRegistrationExceptions("Name is invalid");
 			}
 		}
 		catch (UserRegistrationExceptions e) {
-			throw new UserRegistrationExceptions("Name is invalid");
+			System.out.println(e.toString());
 		}
 	}
 
 	//Uc3 - validate email using regex
-	public String validEMail(String eMail) throws UserRegistrationExceptions {
+	public void validEMail(String eMail) throws UserRegistrationExceptions {
 		String regex = ("^[a-zA-Z0-9]{2,20}([.][a-z0-9]+)?@[a-z]+.[a-z]{2,3}(.[a-z]{2})?$");		
 		
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(eMail);
 		try {
 			if (matcher.matches()) {
-				return "E-mail is valid";
+				System.out.println("E-mail is valid");
 			}
 			else {
 				throw new UserRegistrationExceptions("E-mail is invalid");
 			}
 		}
 		catch (UserRegistrationExceptions e) {
-			throw new UserRegistrationExceptions("E-mail is invalid");
+			System.out.println(e.toString());
 		}
 	}
 	
 	//Uc4,5,6,7- validate password using regex - minimum 8 characters,at least one capital letter,number,special character
-	public String validPassWord(String passWord) throws UserRegistrationExceptions {
+	public void validPassWord(String passWord) throws UserRegistrationExceptions {
 		String regex = ("(?=.*[A-Z]){1,}(?=.*[a-z]){1,}(?=.*[0-9]){1,}(?=.*[@#$%^&-+=()]){1}[a-zA-Z0-9@#$%^&-+=()]{8,}");
 					
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(passWord);
 		try {
 			if (matcher.matches()) {
-				return "Password is valid";
+				System.out.println("Password is valid");
 			}
 			else {
 				throw new UserRegistrationExceptions("Password is invalid");
 			}
 		}
 		catch (UserRegistrationExceptions e) {
-			throw new UserRegistrationExceptions("Password is invalid");
+			System.out.println(e.toString());
 		}
 	}
 	
 	//Uc8 - validate mobile number using regex
-	public String validMobileNumber(String mobileNumber) throws UserRegistrationExceptions {
+	public void validMobileNumber(String mobileNumber) throws UserRegistrationExceptions {
 		String regex = ("[0-9]{2}[\\s][6789]{1}[0-9]{9}");
 					
 		Pattern pattern= Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(mobileNumber);
 		try {
 			if (matcher.matches()) {
-				return "Number is valid";
+				System.out.println("Number is valid");
 			}
 			else {
 				throw new UserRegistrationExceptions("Number is invalid");
 			}
 		}
 		catch (UserRegistrationExceptions e) {
-			throw new UserRegistrationExceptions("Number is invalid");
+			System.out.println(e.toString());
 		}
 	}
 	
